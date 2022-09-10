18241af8dd07ac17832478d1c17aa8d35bafd210
Anthony Lo <anthonylo@Anthonys-MacBook-Pro.local>
2022-09-10 12:12:40 -0700

    jwt refactor

--- routes/gui/api/signIn.tsx
+++ routes/gui/api/signIn.tsx
@@ -1,16 +1,13 @@
-import { MiddlewareHandlerContext } from "$fresh/server.ts";
+import { HandlerContext, Handlers } from "$fresh/server.ts";
 import "https://deno.land/x/dotenv/load.ts";
 
-import { Pool, PoolClient } from "https://deno.land/x/postgres/mod.ts";
+import { Pool } from "https://deno.land/x/postgres/mod.ts";
 import * as cookie from "https://deno.land/std/http/cookie.ts";
-
 import * as bcrypt from "https://deno.land/x/bcrypt/mod.ts";
 import { key } from "../../../utils/key.ts";
 import { create } from "https://deno.land/x/djwt/mod.ts";
-import {
-  QueryArrayResult,
-  QueryObjectResult,
-} from "https://deno.land/x/postgres@v0.16.1/query/query.ts";
+import { getNumericDate } from "https://deno.land/x/djwt@v2.7/mod.ts";
+import { QueryObjectResult } from "https://deno.land/x/postgres@v0.16.1/query/query.ts";
 
 //import * as postgres from "https://deno.land/x/postgres/mod.ts";
 
@@ -27,69 +24,69 @@ interface State {
   data: string;
 }
 
-export const handler: Handler<any, { data: string }> = {
+export const handler: Handlers = {
   async POST(
     req: Request,
     res: Response,
-    ctx: MiddlewareHandlerContext<State>,
-  ) {
-    console.log("in signIn Handler");
-    const body: Ilogin = await req.json();
-    const { username, password } = body;
+    ctx: HandlerContext,
+  ): Promise<Response> {
+    try {
+      console.log("in signIn Handler");
+      const body: Ilogin = await req.json();
+      const { username, password } = body;
 
-    // check if user exists, if user does not exist - return 404
-    const checkUser: QueryObjectResult = await connection.queryObject(
-      `
-      SELECT * FROM users WHERE username = '${username}'
+      // check if user exists, if user does not exist - return 404
+      const checkUser: QueryObjectResult = await connection.queryObject(
+        `
+      SELECT id, username, password FROM users WHERE username = '${username}'
       `,
-    );
-
-    if (checkUser.rows.length > 0) {
-      // TODO: set up JWT and redirect passing JWT
-      const checkPW: boolean = await bcrypt.compare(
-        password,
-        checkUser.rows[0].password,
       );
 
-      if (checkPW) {
-        const payload = {
-          id: checkUser.rows[0].id,
-          username: checkUser.rows[0].username,
-        };
-
-        const jwt: string = await create(
-          { alg: "HS512", typ: "JWT" },
-          { payload },
-          key,
+      if (checkUser.rows.length > 0) {
+        // TODO: set up JWT and redirect passing JWT
+        const checkPW: boolean = await bcrypt.compare(
+          password,
+          checkUser.rows[0].password,
         );
 
-        if (jwt) {
-          const res = new Response(
-            JSON.stringify({
-              id: checkUser.rows[0].id,
-              username: checkUser.rows[0].username,
-              token: jwt,
-            }),
-            { status: 200, headers: new Headers({}) },
+        if (checkPW) {
+          const payload = {
+            id: checkUser.rows[0].id,
+            username: checkUser.rows[0].username,
+            exp: getNumericDate(60 * 60),
+          };
+
+          const jwt: string = await create(
+            { alg: "HS512", typ: "JWT" },
+            { payload },
+            key,
           );
 
-          cookie.setCookie(res.headers, {
-            name: "jwt",
-            value: jwt,
-            httpOnly: true,
-          });
+          if (jwt) {
+            const res = new Response(
+              JSON.stringify({
+                id: checkUser.rows[0].id,
+                username: checkUser.rows[0].username,
+                token: jwt,
+              }),
+              { status: 200, headers: new Headers({}) },
+            );
 
-          return res;
-        } else {
-          return new Response("Please Contact Your Administrator", {
-            status: 404,
-          });
+            cookie.setCookie(res.headers, {
+              name: "jwt",
+              value: jwt,
+              httpOnly: true,
+            });
+
+            return res;
+          }
         }
-      } else {
-        return new Response("Incorrect Credentials", { status: 404 });
       }
-    } else {
-      return new Response("Incorrect Credentials", { status: 404 });
+    } catch (err) {
+      console.log(err);
+      return new Response("Error", { status: 404 });
     }
+
+    return new Response("Invalid Credentials", { status: 404 });
   },
 };
--- utils/key.ts
+++ utils/key.ts
@@ -1,5 +1,5 @@
 export const key = await crypto.subtle.generateKey(
   { name: "HMAC", hash: "SHA-512" },
-  true,
+  false,
   ["sign", "verify"],
 );
