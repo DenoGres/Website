966a4cdf8a85e67251136648cb17cbefee65d47e
Anthony Lo <anthonylo@Anthonys-MacBook-Pro.local>
2022-09-12 17:35:31 -0700

    CRUD connections up

--- islands/Connections.tsx
+++ islands/Connections.tsx
@@ -159,6 +159,7 @@ export default function Connections() {
       const method = (type === "new") ? "POST" : "PATCH";
 
       const reqBody = {
+        connectionId,
         connectionName,
         address,
         port,
