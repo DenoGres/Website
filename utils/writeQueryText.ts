28783c0a31046bd9b8aebbe3d50ea44c4ec48dad
Eddie Wu <eddie.wu.info@gmail.com>
2022-09-17 11:39:03 -0700

    Replace opening double quote from webview with compat version

--- utils/writeQueryText.ts
+++ utils/writeQueryText.ts
@@ -1,6 +1,9 @@
 export default function (uri: string, userQueryStr: string): string {
   // replace any weirdly formatted quotation marks from webview to single quotes
-  userQueryStr = userQueryStr.replaceAll('‘', '\'').replaceAll('’', '\'');
+  userQueryStr = userQueryStr
+    .replaceAll('‘', '\'')
+    .replaceAll('’', '\'')
+    .replaceAll('“', '"');
   const fullQueryString: string = userQueryStr.slice(0, -2) + '\'' + uri + '\'' + userQueryStr.slice(-2);
 
   return `
