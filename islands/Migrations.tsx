683a52ed342d32b5e63bad010e827655c9d5f46b
Anthony Lo <anthonylo@Anthonys-MacBook-Pro.local>
2022-09-16 17:34:31 -0700

    updates to migration

--- islands/Migrations.tsx
+++ islands/Migrations.tsx
@@ -4,15 +4,9 @@ import { useState } from "preact/hooks";
 export default function Migrations() {
   return (
     <div className="w-full flex flex-row">
-      <div className="w-5/12 bg-white rounded mx-3 p-3 items-center">
-        <h2 className="mb-3 text-center">Migration History</h2>
-      </div>
-      <div className="flex flex-col w-full">
-        <div className="flex flex-col h-full bg-white p-3 rounded">
-          <div>
-            <h2>Migration Details</h2>
-          </div>
-        </div>
+      <div className="w-full bg-white rounded mx-3 p-3 items-center">
+        <h2 className="mb-3">Migration History</h2>
+        <p>Coming soon...</p>
       </div>
     </div>
   );
