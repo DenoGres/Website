dfb73dd54320acafbb5f944ff9b72cec259303a3
Mia <mjpgussen@gmail.comn>
2023-01-19 20:00:27 -0500

    remove commented out landing page text

--- routes/index.tsx
+++ routes/index.tsx
@@ -25,24 +25,6 @@ export default function Home() {
         width="500px"
         alt="the DenoGres logo: a blue elephant and a purple dinosaur"
       />
-      {
-        /* <p class="text-2xl text-center text-white">
-        <br />
-        DenoGres is a new Object-Relationship Mapper (ORM) for the Deno runtime
-        environment!
-      </p>
-      <p class=" text-center text-white mx-auto max-w-screen-md">
-        <br />
-        DenoGres implements a Model class which "models" (or represents) your
-        database tables. This class contains functionality which allows you to
-        interact with your database tables without the need for typed queries.
-        Via the DenoGres CLI, it's possible to introspect your database -
-        automatically generating Model sub-classes for each of your database
-        tables in TypeScript! To learn more and get started, dive into our
-        documentation.
-      </p>
-      <br /> */
-      }
     </div>
   );
 }
