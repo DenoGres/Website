cf6beef366c272204f9494b26805ca4289794000
Rachel Cheman <rachelcheman@gmail.com>
2023-01-20 16:16:47 -0500

    removes test file that accidentally got added to routes.

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -12,21 +12,20 @@ import * as $5 from "./routes/docs/methods.tsx";
 import * as $6 from "./routes/docs/migrations.tsx";
 import * as $7 from "./routes/docs/model.tsx";
 import * as $8 from "./routes/docs/seed.tsx";
-import * as $9 from "./routes/docs/test.tsx";
-import * as $10 from "./routes/gui/api/handleConnectionSave.ts";
-import * as $11 from "./routes/gui/api/handleQuerySave.ts";
-import * as $12 from "./routes/gui/api/handleRequests.ts";
-import * as $13 from "./routes/gui/api/logOut.tsx";
-import * as $14 from "./routes/gui/api/setConnectionIdCookie.ts";
-import * as $15 from "./routes/gui/api/signIn.tsx";
-import * as $16 from "./routes/gui/api/signUp.tsx";
-import * as $17 from "./routes/gui/connections.tsx";
-import * as $18 from "./routes/gui/explorer.tsx";
-import * as $19 from "./routes/gui/home.tsx";
-import * as $20 from "./routes/gui/index.tsx";
-import * as $21 from "./routes/gui/logout.tsx";
-import * as $22 from "./routes/gui/migrations.tsx";
-import * as $23 from "./routes/index.tsx";
+import * as $9 from "./routes/gui/api/handleConnectionSave.ts";
+import * as $10 from "./routes/gui/api/handleQuerySave.ts";
+import * as $11 from "./routes/gui/api/handleRequests.ts";
+import * as $12 from "./routes/gui/api/logOut.tsx";
+import * as $13 from "./routes/gui/api/setConnectionIdCookie.ts";
+import * as $14 from "./routes/gui/api/signIn.tsx";
+import * as $15 from "./routes/gui/api/signUp.tsx";
+import * as $16 from "./routes/gui/connections.tsx";
+import * as $17 from "./routes/gui/explorer.tsx";
+import * as $18 from "./routes/gui/home.tsx";
+import * as $19 from "./routes/gui/index.tsx";
+import * as $20 from "./routes/gui/logout.tsx";
+import * as $21 from "./routes/gui/migrations.tsx";
+import * as $22 from "./routes/index.tsx";
 import * as $$0 from "./islands/Connections.tsx";
 import * as $$1 from "./islands/Console.tsx";
 import * as $$2 from "./islands/DocsNav.tsx";
@@ -46,21 +45,20 @@ const manifest = {
     "./routes/docs/migrations.tsx": $6,
     "./routes/docs/model.tsx": $7,
     "./routes/docs/seed.tsx": $8,
-    "./routes/docs/test.tsx": $9,
-    "./routes/gui/api/handleConnectionSave.ts": $10,
-    "./routes/gui/api/handleQuerySave.ts": $11,
-    "./routes/gui/api/handleRequests.ts": $12,
-    "./routes/gui/api/logOut.tsx": $13,
-    "./routes/gui/api/setConnectionIdCookie.ts": $14,
-    "./routes/gui/api/signIn.tsx": $15,
-    "./routes/gui/api/signUp.tsx": $16,
-    "./routes/gui/connections.tsx": $17,
-    "./routes/gui/explorer.tsx": $18,
-    "./routes/gui/home.tsx": $19,
-    "./routes/gui/index.tsx": $20,
-    "./routes/gui/logout.tsx": $21,
-    "./routes/gui/migrations.tsx": $22,
-    "./routes/index.tsx": $23,
+    "./routes/gui/api/handleConnectionSave.ts": $9,
+    "./routes/gui/api/handleQuerySave.ts": $10,
+    "./routes/gui/api/handleRequests.ts": $11,
+    "./routes/gui/api/logOut.tsx": $12,
+    "./routes/gui/api/setConnectionIdCookie.ts": $13,
+    "./routes/gui/api/signIn.tsx": $14,
+    "./routes/gui/api/signUp.tsx": $15,
+    "./routes/gui/connections.tsx": $16,
+    "./routes/gui/explorer.tsx": $17,
+    "./routes/gui/home.tsx": $18,
+    "./routes/gui/index.tsx": $19,
+    "./routes/gui/logout.tsx": $20,
+    "./routes/gui/migrations.tsx": $21,
+    "./routes/index.tsx": $22,
   },
   islands: {
     "./islands/Connections.tsx": $$0,
