affd8dfcccd5a608ed12fc1eeaf30cc9f9e7db42
alexanderlin <alexraptordev@gmail.com>
2023-01-25 19:12:16 -0500

    navbar fix 2

---
+++ islands/NavBarHome.tsx
@@ -0,0 +1,17 @@
+export default function NavBarHome() {
+  const button =
+    "border rounded shadow-md 2xl:px-[1vw] 2xl:py-[0.8vh] mx-4 2xl:text-[1vw] text-base px-4 py-2";
+  return (
+    <nav class="flex justify-start items-center w-full px-5 py-5 text-white">
+      <a class={button + " bg-[#B293B6] ml-auto"} href="/docs/getting-started">
+        Docs
+      </a>
+      <a
+        class={button + " bg-gray-700"}
+        href="https://github.com/open-source-labs/DenoGres"
+      >
+        GitHub
+      </a>
+    </nav>
+  );
+}
